2fc92329b309cda0e2943168d6df6c8ef839983c
Benjamin <60155141+Biitez@users.noreply.github.com>
2020-08-09 19:04:16 -0400

    Add files via upload

---
+++ authGG Manager/Json.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace authGG_Manager
+{
+    #region jsonAttributes
+    public class informationUser
+    {
+        public string status { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string rank { get; set; }
+        public string hwid { get; set; }
+        public string variable { get; set; }
+        public string lastlogin { get; set; }
+        public string lastip { get; set; }
+        public string expiry { get; set; }
+    }
+
+    public class userDelete
+    {
+        public string status { get; set; }
+        public string info { get; set; }
+    }
+
+    public class editVariable
+    {
+        public string status { get; set; }
+        public string info { get; set; }
+    }
+
+    public class userCount
+    {
+        public string status { get; set; }
+        public string value { get; set; }
+    }
+
+    public class unuseLicense
+    {
+        public string status { get; set; }
+        public string info { get; set; }
+    }
+
+    public class useLicense
+    {
+        public string status { get; set; }
+        public string info { get; set; }
+    }
+
+    public class deleteLicense
+    {
+        public string status { get; set; }
+        public string info { get; set; }
+    }
+
+    public class informationLicense
+    {
+        public string status { get; set; }
+        public string license { get; set; }
+        public string rank { get; set; }
+        public string used { get; set; }
+        public string used_by { get; set; }
+        public string created { get; set; }
+    }
+
+    public class licensesCount
+    {
+        public string status { get; set; }
+        public string value { get; set; }
+    }
+
+    public class HWIDInformation
+    {
+        public string status { get; set; }
+        public string value { get; set; }
+    }
+    #endregion
+}
---
+++ authGG Manager/design.cs
@@ -0,0 +1,40 @@
+﻿using Colorful;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Console = Colorful.Console;
+using System.Threading.Tasks;
+
+namespace authGG_Manager
+{
+    #region design
+    public class design
+    {
+        public static Formatter[] colors = new Formatter[]
+        {
+            new Formatter(" [", Color.Gray),
+            new Formatter("!", Color.Violet),
+            new Formatter("]", Color.Gray),
+            new Formatter(" >", Color.Violet),
+            new Formatter("1", Color.Violet),
+            new Formatter("2", Color.Violet),
+            new Formatter("3", Color.Violet),
+            new Formatter("4", Color.Violet),
+            new Formatter("5", Color.Violet),
+            new Formatter("6", Color.Violet),
+            new Formatter("7", Color.Violet),
+        };
+        public static void Logo()
+        {
+            Console.WriteLine("\n\t\t     _         _   _      ____  ____                                               ", Color.BlueViolet);
+            Console.WriteLine("\t\t    / \\  _   _| |_| |__  / ___|/ ___|  _ __ ___   __ _ _ __   __ _  __ _  ___ _ __ ", Color.BlueViolet);
+            Console.WriteLine("\t\t   / _ \\| | | | __| '_ \\| |  _| |  _  | '_ ` _ \\ / _` | '_ \\ / _` |/ _` |/ _ \\ '__|", Color.BlueViolet);
+            Console.WriteLine("\t\t  / ___ \\ |_| | |_| | | | |_| | |_| | | | | | | | (_| | | | | (_| | (_| |  __/ |   ", Color.BlueViolet);
+            Console.WriteLine("\t\t /_/   \\_\\__,_|\\__|_| |_|\\____|\\____| |_| |_| |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   ", Color.BlueViolet);
+            Console.WriteLine("\t\t                                                                   |___/           ", Color.BlueViolet);
+        }
+    }
+    #endregion
+}
